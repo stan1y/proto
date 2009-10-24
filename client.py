bf96135bf03076e67e9f84494546240c18abdea3
Stanislav Yudin <decvar@gmail.com>
2009-10-24 18:47:01 +0530

    try two methods

--- client.py
+++ client.py
@@ -5,11 +5,12 @@ Created on  Oct 23, 2009
 
 @author: Stanislav Yudin
 '''
-#proto library
-from proto import ProtoChannel, ProtoController, ProtoError, set_module
-#user
-import k7talk_pb2 as k7mod
-from k7talk_pb2 import *
+#proto library imports
+from proto import ProtoChannel, ProtoController, ProtoError, set_pb2_module
+#pb2 generated module
+import k7talk_pb2 as pb2
+#user imports
+from k7talk_pb2 import K7_Login, K7TalkServer_Stub
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
@@ -19,18 +20,24 @@ def async_callback(answer):
 	assert not answer == None
 
 if __name__ == '__main__':
-	log.info('client starting')
-	server = K7TalkServer_Stub(ProtoChannel('localhost', 9999))
-	set_module(k7mod)
+	print 'client starting'
+	#specifying generated classes
+	server = K7TalkServer_Stub(ProtoChannel('localhost', 9999, pb2))
 	controller = ProtoController()
-	login_ = K7_Login()
-	login_.login = 'asd'
-	login_.password = 'zxc'
 	try:
+		login_ = K7_Login()
+		login_.login = 'asd'
+		login_.password = 'zxc'
 		user_info  = server.login(controller, login_, async_callback)
-		log.info('answer' + str(user_info))
-		user_info  = server.login(controller, login_, async_callback)
-		log.info('answer' + str(user_info))
-		log.info('client done')
+		print 'login answer %s' % str(user_info)
+		
+		
+		s_ = K7_SignIn()
+		s_.app_name = 'my-app'
+		s_.user = user_info
+		app_info  = server.signin_app(controller, login_, async_callback)
+		print 'signin_app %s' % app_info
+		
+		print 'client done'
 	except ProtoError, pe:
-		log.error(str(pe))
+		print str(pe)
