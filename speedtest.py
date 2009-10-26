ff1db3a2a077ddfe0c2c6fa8ea8c3ae686859c85
Stanislav Yudin <decvar@gmail.com>
2009-10-26 21:23:28 +0530

    port speedtest to protocol

--- speedtest.py
+++ speedtest.py
@@ -5,14 +5,11 @@ Created on Oct 25, 2009
 '''
 
 import sys, logging, os, datetime, time
-from proto import run_server, set_pb2_module
-from k7talk_pb2 import *
-import k7talk_pb2 as pb2
-import k7impl as impl
+import proto
+import sample_pb2
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(filename='speedtest.log', level=logging.DEBUG)
 log = logging.getLogger(__name__)
-
 
 def test_10_calls():
 	run(10)
@@ -23,39 +20,36 @@ def test_1000_calls():
 
 def do_call(number):
 	results = {}
-	server = K7TalkServer_Stub(ProtoChannel('localhost', 9999, pb2))
-	controller = ProtoController()
-	login_ = K7_Login()
-	login_.login = 'asd'
-	login_.password = 'zxc'
+	server = sample_pb2.sample_rpc_Stub(proto.ProtoChannel('localhost', 9999, sample_pb2))
+	controller = proto.ProtoController()
 	for i in xrange(1, number):
 		before = datetime.time.now()
-		user_info  = server.login(controller, login_, async_callback)
-		after = datetime.time.now()
+		rst = sample_pb2.sample_request( message = 'hello server!')
+		assert server.the_method(controller, rst, async_callback) != None
 		results[i] = after - before
 	log.info('calculating...')
 	total = long(0)
 	for i in xrange(1, number):
 		total += results[i]
+	assert total > 0
 	return total / len(results)
 
 def run(number):
 	log.info('Starting speed test')
 	pid = os.fork()
+	error = None
 	if pid:
 		try:
 			log.info('waiting 5 seconds to let server start...')
 			time.sleep(5)
 			log.info('testing %s calls' % number)
 			log.info('result: %s' % do_call(number))
-		except ProtoError, pe:
-			print 'ProtoError:', str(pe)
-			sys.exit(1)
-		
+		finally:
+			os.kill(pid)
 	else:
 		#child runs server
 		port = 9999
 		if len(sys.argv) == 2:
 			port = int(sys.argv[1])
-		run_server(port, pb2, impl)
+		sample_impl.run_server(port, sample_pb2)
 		
