84934e76332b5108d7881d12b80496fd2ff8706d
Stanislav Yudin <Stanly@Silverblaze.local>
2010-01-14 18:47:03 +0530

    improved setup, support bdist_egg for distribution

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
 
 setup(
 		name='proto',
 		version='0.3.1',
-		packages=['proto']
-		description = "The Proto! Python Async RPC based on ProtocolBuffers and TCP sockets.",
-		author = "Stanislav Yudin"
-		author_email = "decvar@gmail.com",
-		classifiers = ["Development Status :: Beta", "Framework :: ProtocolBuffers"],
-		include_package_data = True,
+		description="The \"Proto!\" Python Async RPC based on ProtocolBuffers and TCP sockets.",
+		author="Stanislav Yudin",
+		author_email="decvar@gmail.com",
+		url="http://github.com/AwesomeStanly/proto",
+		classifiers=["Development Status :: Beta", "Framework :: ProtocolBuffers"],
+		include_package_data=True,
+		install_requires=[ "protobuf>=2.2.0a" ],
+		packages=['proto'],
+		setup_requires=[],
+		zip_safe=True
 	)
